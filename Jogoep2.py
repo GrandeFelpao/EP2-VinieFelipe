1e8acaf55b25c3525ce036a3ea8e56a0f7d0233b
viniciusbp7 <vinicius.b.pomilio@gmail.com>
2023-04-19 14:47:20 -0300

    Ex 2 preenche frota

--- Jogoep2.py
+++ Jogoep2.py
@@ -1,3 +1,4 @@
+# Define posições-------------------------------------------------------------------------------------------------
 def define_posicoes(linha, coluna, orientacao, tamanho):
 
     coordenadas = []
@@ -11,3 +12,30 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             coluna += 1
 
     return coordenadas
+
+# Preenche frota--------------------------------------------------------------------------------------------------
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    coordenadas = []
+    for i in range(int(tamanho)):
+        coordenadas.append([linha, coluna])
+        if orientacao == "vertical":
+            linha += 1
+        if orientacao=="horizontal":
+            coluna += 1
+    return coordenadas
+
+
+def preenche_frota(frota,nome_do_navio,linha, coluna, orientacao, tamanho):
+    nova_posicao=[]
+    nova_posicao.append(define_posicoes(linha, coluna, orientacao, tamanho))
+
+    if nome_do_navio not in frota:
+        frota[nome_do_navio]=nova_posicao
+
+    else:
+        posicao=frota[nome_do_navio]
+        posicao.append(define_posicoes(linha, coluna, orientacao, tamanho))
+        frota[nome_do_navio]=posicao
+    return frota
+
